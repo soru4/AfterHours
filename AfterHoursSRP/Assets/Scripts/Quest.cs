562f199af4b2ca116c1189db900de088473f5892
soru4 <sohumtambe1@gmail.com>
2024-04-19 12:02:49 -0500

    quest, scripts

---
+++ AfterHoursSRP/Assets/Scripts/Quest.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Quest", menuName = "ScriptableObjects/Quest", order = 1)]
+
+public class Quest : ScriptableObject
+{
+    public string questName = string.Empty;
+    public Vector3 questStartPos = Vector3.zero;
+    public float questRadius = 5;
+    public List<string> questDialouges = new List<string>();
+
+}
---
+++ AfterHoursSRP/Assets/Scripts/QuestManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    public List<Quest> quests = new List<Quest>();
+    public Quest activeQuest = null;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
